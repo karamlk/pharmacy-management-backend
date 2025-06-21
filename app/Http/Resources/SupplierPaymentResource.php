6c3e26041d21ee049098c33b42b25afccae4c97c
Kar <kargit@gmail.com>
2025-06-21 22:17:37 +0300

    Handling the supplier payments logic

---
+++ app/Http/Resources/SupplierPaymentResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SupplierPaymentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'supplier_name'  => $this->supplier?->name,
+            'processed_by'   => $this->user?->name,
+            'amount_paid'    => $this->amount,
+            'payment_date'   => $this->payment_date,
+        ];
+    }
+}
