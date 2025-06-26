16a17a544939bbf0fbbe11d21ef07e51dbcb7009
Kar <karam.alrifai24@gmail.com>
2025-06-26 19:01:27 +0300

    Crud for pharmacist info's from the admin side and Profile management

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->decimal('hourly_rate', 8, 2)->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->softDeletes();
