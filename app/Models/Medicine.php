d64a0c061520bc0a7a3b08878d207161a70f3500
Kar <kargit@gmail.com>
2025-06-30 02:15:53 +0300

    Implement expired and out-of-stock medicines endpoints

--- app/Models/Medicine.php
+++ app/Models/Medicine.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -10,11 +11,16 @@ class Medicine extends Model
 {
     use SoftDeletes;
 
-    protected $fillable = ['category_id', 'name','barcode','manufacturer', 'active_ingredient', 'price', 'quantity', 'production_date', 'expiry_date', 'img_url'];
+    protected $fillable = ['category_id', 'name', 'barcode', 'manufacturer', 'active_ingredient', 'price', 'quantity', 'production_date', 'expiry_date', 'img_url'];
 
     public function category(): BelongsTo
     {
         return  $this->belongsTo(Category::class);
     }
 
+    public function scopeAvailable(Builder $query)
+    {
+        return $query->where('quantity', '>', 0)
+            ->whereDate('expiry_date', '>=', now()->toDateString());
+    }
 }
